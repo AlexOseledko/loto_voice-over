ee8df9e5cf50ebc549da584f7dbbdc030b8829f2
Alexey Oseledko <AlexeyOseledko@gmail.com>
2021-01-24 16:32:35 +0200

    Add files via upload

---
+++ js/speak.js
@@ -0,0 +1,82 @@
+var synth = window.speechSynthesis;
+var inputForm = document.querySelector('form');
+var voiceSelect = document.querySelector('select');
+
+var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');
+
+var errAudio = new Audio('./assets/err.mp3');
+
+var voices = [];
+
+function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {
+        const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+        if (aname < bname) return -1;
+        else if (aname == bname) return 0;
+        else return +1;
+    });
+    var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = '';
+    for (i = 0; i < voices.length; i++) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = 12;
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function speak(val) {
+    text = val + '';
+    if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        errAudio.play();
+        setTimeout(() => {
+            errAudio.pause();
+            errAudio.currentTime = 0.0;
+        }, 1000)
+        return;
+    }
+    if (text !== '') {
+        var utterThis = new SpeechSynthesisUtterance(text);
+        utterThis.onend = function (event) {
+            console.log('SpeechSynthesisUtterance.onend');
+        }
+        utterThis.onerror = function (event) {
+            console.error('SpeechSynthesisUtterance.onerror');
+        }
+        var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+        for (i = 0; i < voices.length; i++) {
+
+            if (voices[i].name === selectedOption) {
+                utterThis.voice = voices[i];
+                break;
+            }
+        }
+        utterThis.pitch = pitch.value;
+        utterThis.rate = rate.value;
+        synth.speak(utterThis);
+    }
+}
+
+pitch.onchange = function () {
+    pitchValue.textContent = pitch.value;
+}
+
+rate.onchange = function () {
+    rateValue.textContent = rate.value;
+}
+
+voiceSelect.onchange = function () {
+    speak();
+}
